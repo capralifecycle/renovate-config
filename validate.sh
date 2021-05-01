2e70f38b64548f8aaebea89615eec77d4524bac4
Henrik Steen <henrist@henrist.net>
2021-05-01 22:18:27 +0200

    Add ajv-formats and migrate schema during validation

--- validate.sh
+++ validate.sh
@@ -4,10 +4,14 @@ set -eu -o pipefail
 echo "Fetching latest JSON Schema"
 curl -s https://docs.renovatebot.com/renovate-schema.json > renovate-schema.json
 
+# As of this writing, Renovate uses draft-04 which is
+# too old for the validate command of ajv.
+echo "Migrating schema to newer version"
+npx ajv migrate -s renovate-schema.json
+
 echo "Now validating.."
-echo "Note: The logs for 'keywords ignored' can be ignored."
 
 for x in $(ls -1 *.json | grep -v "^package.json$" | grep -v "^package-lock.json$"); do
   echo "Validating $x"
-  npx ajv -s renovate-schema.json -d "$x"
+  npx ajv -s renovate-schema.json -d "$x" -c ajv-formats
 done
