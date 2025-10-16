848617fe696d9ffb98c7069dcdce1752c397ef4a
Joakim L. Engeset <joakim.engeset@gmail.com>
2025-10-16 14:15:17 +0200

    fix: ignore vendor-specific keywords in validation

--- validate.sh
+++ validate.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+SCHEMA="renovate-schema.json"
+
 echo "Fetching latest JSON Schema"
-curl -s https://docs.renovatebot.com/renovate-schema.json >renovate-schema.json
+curl -s "https://docs.renovatebot.com/$SCHEMA" > "$SCHEMA"
 
 # As of this writing, Renovate uses draft-04 which is
 # too old for the validate command of ajv.
-echo "Migrating schema to newer version"
-npx ajv migrate -s renovate-schema.json
+echo "Migrating schema [${SCHEMA}] to newer version"
+npx ajv migrate -s "$SCHEMA"
 
 echo "Now validating.."
 
 for x in *.json; do
   if [[ "$x" != "package.json" && "$x" != "package-lock.json" ]]; then
-    echo "Validating $x"
-    npx ajv -s renovate-schema.json -d "$x" -c ajv-formats
+    echo "Validating $x .."
+    npx ajv -s "$SCHEMA" -d "$x" -c ajv-formats --strict-schema=false
   fi
 done
